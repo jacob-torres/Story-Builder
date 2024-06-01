660854c1c038acc557ea737f4690c2c107fa3f07
Jacob Torres <contact@jacobtorres.net>
2024-06-01 17:41:40 -0600

    Included psycopg2-binary to requirements file.

--- storybuilder/storybuilder/settings.py
+++ storybuilder/storybuilder/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import environ
 from pathlib import Path
+
+# Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
