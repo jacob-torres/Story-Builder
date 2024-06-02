55fd6cc1c30426ab6812b91abe0670827157a357
Jacob Torres <contact@jacobtorres.net>
2024-06-02 03:08:17 -0600

    Minor bug fix in models module.

--- storybuilder/app/models.py
+++ storybuilder/app/models.py
@@ -42,7 +42,7 @@ class Idea(models.Model):
         help_text='Upload an image to visualize your idea.'
     )
 
-    idea_links = JSONField(
+    idea_links = models.JSONField(
         db_column='idea_links',
         null=True,
         help_text='A list of links to your idea references.'
@@ -72,7 +72,7 @@ class Story(models.Model):
         help_text='Enter your story title.'
     )
 
-    story_ideas = JSONField(
+    story_ideas = models.JSONField(
         db_column='story_ideas',
         null=True,
         help_text='The list of ideas which make up your story.'
