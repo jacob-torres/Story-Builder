4235a046d8a83c3b0c9587417c26488e903b8149
Jacob Torres <contact@jacobtorres.net>
2024-11-09 12:27:33 -0700

    Updated Character model, created Story, Plot, PlotPoint, Scene, and World models.

--- storybuilder/app/models.py
+++ storybuilder/app/models.py
@@ -4,7 +4,22 @@ from django.db import models
 max_length = 100
 
 
+class Story(models.Model):
+    """The largest datatype, having characters, plots, worlds, and scenes."""
+
+    # Story details
+    title = models.CharField(max_length=max_length)
+    premise = models.TextField()
+    synopsis = models.TextField()
+    genre = models.CharField(max_length=max_length)
+    word_count = models.PositiveIntegerField(default=0)
+    date_started = models.DateField(null=False, auto_now_add=True)
+    date_last_saved = models.DateField(null=False, auto_now=True)
+    date_finished = models.DateField(null=True, blank=True)
+
+
 class Character(models.Model):
+    """Story character."""
 
     # Names
     first_name = models.CharField(max_length=max_length)
@@ -16,6 +31,7 @@ class Character(models.Model):
     age = models.PositiveSmallIntegerField()
     ethnicity = models.CharField(max_length=max_length)
     occupation = models.CharField(max_length=max_length)
+    location = models.CharField(max_length=max_length)
 
     # Physical details
     hair_color = models.CharField(max_length=max_length)
@@ -41,18 +57,44 @@ class Character(models.Model):
     description = models.TextField()
 
 
+class Scene(models.Model):
+    """A single unit or building block of a story."""
+
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    # Relationships: One story, one or more characters
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    characters = models.ManyToManyField(Character)
+
+
+class PlotPoint(models.Model):
+    """A single point of a story's plot."""
+
+    name = models.CharField(max_length=max_length, blank=False)
+    description = models.TextField(blank=True)
+
+    # Relationships: One or more scenes
+    scenes = models.ManyToManyField(Scene)
+
+
 class Plot(models.Model):
+    """Plots and their plot points, characters, and progressions."""
+
     title = models.CharField(max_length=200)
     synopsis = models.TextField()
-    # ... other fields
+
+    # Relationships: One story, many plot points
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+    plot_points = models.ManyToManyField(PlotPoint)
+
 
 class World(models.Model):
+    """Worlds and their details."""
+
     name = models.CharField(max_length=100)
     description = models.TextField()
-    # ... other fields
 
-class Scene(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    plot = models.ForeignKey(Plot, on_delete=models.CASCADE)
-    # ... other fields
+    # Relationships: One or more stories and characters
+    stories = models.ManyToManyField(Story)
+    characters = models.ManyToManyField(Character)
