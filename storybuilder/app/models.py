8feba45ce9dd133c5c50947b1dcaeab90fd5d6fb
Jacob Torres <contact@jacobtorres.net>
2024-11-09 15:36:41 -0700

    Fixed personality type choices in models module.

--- storybuilder/app/models.py
+++ storybuilder/app/models.py
@@ -24,19 +24,39 @@ class Character(models.Model):
     height = models.CharField(max_length=max_length, blank=True)
     body_type = models.CharField(max_length=max_length, blank=True)
 
-    # Generate MBTI personality type combinations
-    mbti_choices = []
-    for e_i in ['E', 'I']:
-        for n_s in ['N', 'S']:
-            for f_t in ['F', 'T']:
-                for j_p in ['J', 'P']:
-                    mbti_choices.append(e_i + n_s + f_t + j_p)
+    # MBTI personality types
+    mbti_choices = [
+        ('INTJ', 'INTJ: The Architect'),
+        ('INTP', 'INTP: The Logician'),
+        ('ENTJ', 'ENTJ: The Commander'),
+        ('ENTP', 'ENTP: The Visionary'),
+        ('INFJ', 'INFJ: The Advocate'),
+        ('INFP', 'INFP: The Idealist'),
+        ('ENFJ', 'ENFJ: The Giver'),
+        ('ENFP', 'ENFP: The Enthusiast'),
+        ('ISTJ', 'ISTJ: The Duty Fulfiller'),
+        ('ISFJ', 'ISFJ: The Protector'),
+        ('ESTJ', 'ESTJ: The Executive'),
+        ('ESFJ', 'ESFJ: The Caregiver'),
+        ('ISTP', 'ISTP: The Craftsman'),
+        ('ISFP', 'ISFP: The Artist')
+    ]
 
-    # Generate enneagram types 1 through 9
-    enneagram_choices = [num + 1 for num in range(9)]
+    # Enneagram personality types
+    enneagram_choices = [
+        ('1', '1: The Reformer'),
+        ('2', '2: The Helper'),
+        ('3', '3: The Achiever'),
+        ('4', '4: The Romantic'),
+        ('5', '5: The Investigator'),
+        ('6', '6: The Skeptic'),
+        ('7', '7: The Enthusiast'),
+        ('8', '8: The Challenger'),
+        ('9', '9: The Peacemaker')
+    ]
 
-    mbti_personality = models.CharField(choices=mbti_choices, max_length=4, null=True)
-    enneagram_personality = models.PositiveSmallIntegerField(choices=enneagram_choices, null=True)
+    mbti_personality = models.CharField(choices=mbti_choices, null=True)
+    enneagram_personality = models.CharField(choices=enneagram_choices, null=True)
 
     # Long character description
     description = models.TextField(blank=True)
