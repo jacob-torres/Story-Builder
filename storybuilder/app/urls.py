0784a8d0cdd3c02a8f24e1f301f15d9cbc1770d2
Jacob Torres <contact@jacobtorres.net>
2024-11-16 20:28:21 -0700

    Added new_story URL, view function, and template, as well as stories URL.

--- storybuilder/app/urls.py
+++ storybuilder/app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path    ('stories', views.stories, name='stories'),
+    path('stories/new/', views.new_story, name='new_story')
 ]
--- storybuilder/app/views.py
+++ storybuilder/app/views.py
@@ -6,3 +6,15 @@ from django.http import HttpResponse
 def home(request):
     """Home page."""
     return render(request, 'home.html')
+
+
+def stories(request):
+    """View function for listing stories."""
+
+    return render(request, 'stories.html')
+
+
+def new_story(request):
+    """View function for creating a new story."""
+
+    return render(request, 'new_story.html')
