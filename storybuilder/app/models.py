acec124b5b69ea083acc734b97ad2bb72cbcab92
Jacob Torres <contact@jacobtorres.net>
2024-11-16 19:35:17 -0700

    Updated models module class relationships.

--- storybuilder/app/models.py
+++ storybuilder/app/models.py
@@ -1,28 +1,36 @@
 from django.db import models
 
 # Create your models here.
-max_length = 100
+max_length = 250
+
+
+class Collection(models.Model):
+    """Collection of stories."""
+
+    name = models.CharField(max_length=max_length, null=False)
+    description = models.TextField(null=True)
+    num_stories = models.PositiveSmallIntegerField(default=0)
 
 
 class Character(models.Model):
     """Story character."""
 
-    first_name = models.CharField(max_length=max_length, blank=False)
-    middle_name = models.CharField(max_length=max_length, blank=True)
-    last_name = models.CharField(max_length=max_length, blank=True)
+    first_name = models.CharField(max_length=max_length, null=False)
+    middle_name = models.CharField(max_length=max_length, null=True)
+    last_name = models.CharField(max_length=max_length, null=True)
 
     # Demographic details
-    gender = models.CharField(max_length=max_length, blank=True)
+    gender = models.CharField(max_length=max_length, null=True)
     age = models.PositiveSmallIntegerField(null=True)
-    ethnicity = models.CharField(max_length=max_length, blank=True)
-    occupation = models.CharField(max_length=max_length, blank=True)
-    location = models.CharField(max_length=max_length, blank=True)
+    ethnicity = models.CharField(max_length=max_length, null=True)
+    occupation = models.CharField(max_length=max_length, null=True)
+    location = models.CharField(max_length=max_length, null=True)
 
     # Physical details
-    hair_color = models.CharField(max_length=max_length, blank=True)
-    eye_color = models.CharField(max_length=max_length, blank=True)
-    height = models.CharField(max_length=max_length, blank=True)
-    body_type = models.CharField(max_length=max_length, blank=True)
+    hair_color = models.CharField(max_length=max_length, null=True)
+    eye_color = models.CharField(max_length=max_length, null=True)
+    height = models.CharField(max_length=max_length, null=True)
+    body_type = models.CharField(max_length=max_length, null=True)
 
     # MBTI personality types
     mbti_choices = [
@@ -59,74 +67,65 @@ class Character(models.Model):
     enneagram_personality = models.CharField(choices=enneagram_choices, null=True)
 
     # Long character description
-    description = models.TextField(blank=True)
+    description = models.TextField(null=True)
 
 
 class Story(models.Model):
     """The story data structure, with characters, plots, worlds, and scenes."""
 
     # Story details
-    title = models.CharField(max_length=max_length, blank=False)
-    premise = models.TextField(blank=True)
-    description = models.TextField(blank=True)
-    genre = models.CharField(max_length=max_length, blank=True)
+    title = models.CharField(max_length=max_length, null=False)
+    premise = models.TextField(null=True)
+    description = models.TextField(null=True)
+    genre = models.CharField(max_length=max_length, null=True)
     word_count = models.PositiveIntegerField(default=0)
     date_started = models.DateField(auto_now_add=True)
     date_last_saved = models.DateField(auto_now=True)
     date_finished = models.DateField(null=True)
 
-    # Relationships: One or more characters, plots, and scenes
+    # Relationships: One collection, one or more characters
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
     characters = models.ManyToManyField(Character)
 
 
 class Scene(models.Model):
     """A single unit or building block of a story."""
 
-    title = models.CharField(max_length=max_length, blank=False)
-    description = models.TextField(blank=True)
+    title = models.CharField(max_length=max_length, null=False)
+    description = models.TextField(null=True)
 
     # Relationships: One story, one or more characters
     story = models.ForeignKey(Story, on_delete=models.CASCADE)
     characters = models.ManyToManyField(Character)
 
 
-class PlotPoint(models.Model):
-    """A single point of a story's plot."""
-
-    name = models.CharField(max_length=max_length, blank=False)
-    description = models.TextField(blank=True)
-
-    # Relationships: One or more scenes
-    scenes = models.ManyToManyField(Scene)
-
-
 class Plot(models.Model):
     """Plots and their plot points, characters, and progressions."""
 
-    name = models.CharField(max_length=max_length, blank=False)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=max_length, null=False)
+    description = models.TextField(null=True)
 
-    # Relationships: One or more stories, many plot points
-    stories = models.ManyToManyField(Story)
-    plot_points = models.ManyToManyField(PlotPoint)
+    # Relationships: One story
+    story = models.ForeignKey(Story, on_delete=models.CASCADE)
+
+
+class PlotPoint(models.Model):
+    """A single point of a story's plot."""
+
+    name = models.CharField(max_length=max_length, null=False)
+    description = models.TextField(null=True)
+
+    # Relationships: One plot, one or more scenes per plot point
+    plot = models.ForeignKey(Plot, on_delete=models.CASCADE)
+    scenes = models.ManyToManyField(Scene)
 
 
 class World(models.Model):
     """Worlds and their details."""
 
-    name = models.CharField(max_length=max_length, blank=False)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=max_length, null=False)
+    description = models.TextField(null=True)
 
     # Relationships: One or more stories and characters
     stories = models.ManyToManyField(Story)
     characters = models.ManyToManyField(Character)
-
-
-class Collection(models.Model):
-    """Collection of stories."""
-
-    name = models.CharField(max_length=max_length, blank=False)
-    description = models.TextField(blank=True)
-
-    # Relationships: One or more stories
-    stories = models.ManyToManyField(Story)
