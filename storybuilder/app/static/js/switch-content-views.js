d31624fe999f0f784d1bd34c694ffb786bb4c7e9
Jacob Torres <contact@jacobtorres.net>
2024-11-30 15:10:10 -0700

    JavaScript now allowing scene and character count links in the detail table to switch and jump to the corresponding view.

--- storybuilder/app/static/js/switch-content-views.js
+++ storybuilder/app/static/js/switch-content-views.js
@@ -1,12 +1,26 @@
 // For switching between content views
-const navTabs = document.querySelectorAll('.view-tab');
-const contentDivs = document.querySelectorAll('.content-view')
+const viewLinks = document.querySelectorAll('.view-link');
+const viewTabs = document.querySelectorAll('.view-tab');
+const contentDivs = document.querySelectorAll('.content-view');
 
-navTabs.forEach((link, index) => {
+// Clicking on a link in the details exposes and jumps to the corresponding view
+viewLinks.forEach((link, index) => {
+    link.addEventListener('click', () => {
+
+        // Hide all content views
+        contentDivs.forEach(div => div.style.display = 'none');
+
+        // Display content view corresponding with the clicked link
+        contentDivs[index].style.display = 'block';
+    });
+});
+
+// Clicking on a tab heading activates the corresponding view
+viewTabs.forEach((link, index) => {
     link.addEventListener('click', () => {
 
         // Remove the active class from each tab
-        navTabs.forEach(link => link.classList.remove('active'));
+        viewTabs.forEach(link => link.classList.remove('active'));
 
         // Add the active class back to the tab corresponding to the clicked link
         link.classList.add('active');
