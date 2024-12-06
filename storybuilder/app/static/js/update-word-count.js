37dd49ecd6792f43078fb833eecb1da21a339da5
Jacob Torres <contact@jacobtorres.net>
2024-12-05 18:19:25 -0700

    Updated story_detail page so that the word count is a clickable link that jumps to the word count form.

--- storybuilder/app/static/js/update-word-count.js
+++ storybuilder/app/static/js/update-word-count.js
@@ -1,5 +1,14 @@
-// Update story word count
+// Update word count
+const wordCountLink = document.getElementById('word-count-link');
+const wordCountForm = document.getElementById('word-count-form')
+
+// Toggle form visibility for updating story word count
 function toggleWordCountForm() {
     var form = document.getElementById('word-count-form');
     form.style.display = form.style.display === 'none' ? 'block' : 'none';
 }
+
+// Jump to word count form when clicking certain links
+wordCountLink.addEventListener('click', () => {
+    wordCountForm.style.display = 'block';
+});
