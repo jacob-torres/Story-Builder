045305e90a4e58137466fd13c854cfc066dd7797
Jacob Torres <contact@jacobtorres.net>
2025-01-06 11:43:23 -0700

    Copied templates and static directories into app directory.

---
+++ storybuilder/app/static/js/add-scene-note.js
@@ -0,0 +1,7 @@
+// Add a note to a scene
+const sceneNoteForm = document.getElementById('scene-note-form');
+
+// Toggle visibility of the form
+function toggleSceneNoteForm() {
+    sceneNoteForm.style.display = sceneNoteForm.style.display === 'none' ? 'block' : 'none';
+}
