dc0b9df2c49a4843d9d8269fa4fa21ee2ce621ee
Jacob Torres <contact@jacobtorres.net>
2025-01-09 16:43:19 -0700

    Added login and registration functionality; issues with logging in new user.

--- storybuilder/accounts/urls.py
+++ storybuilder/accounts/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('profile/', views.profile, name='profile'),
+    path('profile/update/', views.update_profile, name='update_profile')
 ]
