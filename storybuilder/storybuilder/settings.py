be139ad1fae799e368ab348122c3cf442f78d788
Jacob Torres <contact@jacobtorres.net>
2025-02-06 12:27:23 -0700

    Set SSL and cookie security settings.

--- storybuilder/storybuilder/settings.py
+++ storybuilder/storybuilder/settings.py
@@ -134,3 +134,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Security Settings
+
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE = True
+CSRF_COOKIE_SECURE = True
