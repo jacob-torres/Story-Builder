0cb684bb22fabaf22c24e36fe2ffa6fa04a469a2
Jacob Torres <contact@jacobtorres.net>
2025-02-24 17:55:02 -0700

    Initial view test case.

---
+++ storybuilder/app/tests/test_views.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from django.http import HttpResponse
+
+from app.views import *
+
+
+class ViewTestCase(TestCase):
+    """Test case for app view functions."""
+
+
+    ### Setup
+
+    def setUp(self):
+        return super().setUp()
+
+
+    ### Teardown
+
+    def tearDown(self):
+        return super().tearDown()
