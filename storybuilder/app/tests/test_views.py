17a14dc7238b4ee7d9ebaf0fd3b1d4e7c6ada32f
Jacob Torres <contact@jacobtorres.net>
2025-02-24 20:37:59 -0700

    Added test module for view tests and initial home get request test.

--- storybuilder/app/tests/test_views.py
+++ storybuilder/app/tests/test_views.py
@@ -1,7 +1,6 @@
-from django.test import TestCase
-from django.http import HttpResponse
+from django.test import TestCase, Client
 
-from app.views import *
+from accounts.models import CustomUser
 
 
 class ViewTestCase(TestCase):
@@ -11,10 +10,58 @@ class ViewTestCase(TestCase):
     ### Setup
 
     def setUp(self):
+
+        print("View Test Setup")
+
+        # Create user object
+        self.user = CustomUser.objects.create(
+            username='writer5000',
+            password='1VerySecurePassword!',
+            email='example@email.com',
+            first_name='Jack',
+            last_name='Writer'
+        )
+
+        # Create client instance for sending URL requests
+        self.client = Client()
+
         return super().setUp()
+    
+
+    def test_view_home_get(self):
+        """Test for the home view function."""
+
+        print("\n***********************\n")
+        print("Testing the home view function\n")
+
+        # Create get request for the home view
+        print("Get request to the home view with no user logged in")
+        response = self.client.get('/')
+
+        # Test response status
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>Welcome to The Story Builder', response.content)
+
+        # Create get request for the home view with user logged in
+        print("Get request to the home view with user logged in")
+        self.client.force_login(self.user)
+        response = self.client.get('/')
+
+        # Test status code
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'<h1>Welcome back, Jack!</h1>', response.content)
+
+        # Log out user
+        self.client.logout()
 
 
     ### Teardown
 
     def tearDown(self):
+
+        print("View Test Teardown")
+
+        # Log out user from client
+        self.client.logout()
+
         return super().tearDown()
