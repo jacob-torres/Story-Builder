a5ffa3176fb1b3f6a5426ff65eb5cf686404b434
Jacob Torres <contact@jacobtorres.net>
2025-03-02 13:10:42 -0700

    Updated settings to include localhost and testserver as allowed hosts.

--- storybuilder/storybuilder/settings.py
+++ storybuilder/storybuilder/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'testserver'
+]
 
 
 # Application definition
